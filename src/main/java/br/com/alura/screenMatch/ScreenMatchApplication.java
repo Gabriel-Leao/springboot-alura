7166e0a056d62575bb6bb07ea64bf2e04affb8ee
Gabriel-Leao <gabriel.leao2507@gmail.com>
2025-01-14 16:40:07 -0300

    feat: using envs for db connection

--- src/main/java/br/com/alura/screenMatch/ScreenMatchApplication.java
+++ src/main/java/br/com/alura/screenMatch/ScreenMatchApplication.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenMatch;
 
 import br.com.alura.screenMatch.main.Main;
+import br.com.alura.screenMatch.utils.EnvUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,6 +18,11 @@ public class ScreenMatchApplication implements CommandLineRunner {
 	}
 
 	public static void main(String[] args) {
+		System.setProperty("DB_HOST", EnvUtil.getEnv("DB_HOST"));
+		System.setProperty("DB_NAME", EnvUtil.getEnv("DB_NAME"));
+		System.setProperty("DB_USER", EnvUtil.getEnv("DB_USER"));
+		System.setProperty("DB_PASSWORD", EnvUtil.getEnv("DB_PASSWORD"));
+
 		SpringApplication.run(ScreenMatchApplication.class, args);
 	}
 
